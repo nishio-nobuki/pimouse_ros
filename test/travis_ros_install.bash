da7f888e7a868ef34fda7830fc47105572e26837
nishio-nobuki <nn.symmetry@outlook.com>
2017-11-06 16:55:59 +0000

    use ubuntu14 and sudo in travis_ros_install.bash

--- test/travis_ros_install.bash
+++ test/travis_ros_install.bash
@@ -1,15 +1,15 @@
 #!/bin/bash -xve
 
 #required packages
-pip install catkin_pkg
-pip install empy
-pip install pyyaml
-pip install rospkg
+sudo pip install catkin_pkg
+sudo pip install empy
+sudo pip install pyyaml
+sudo pip install rospkg
 
 #ros install
 cd ..
-git clone https://github.com/nishio-nobuki/ros_setup_scripts_Ubuntu16.04_server.git
-cd ./ros_setup_scripts_Ubuntu16.04_server
+git clone https://github.com/nishio-nobuki/ros_setup_scripts_Ubuntu14.04_server.git
+cd ./ros_setup_scripts_Ubuntu14.04_server
 bash ./step0.bash
 bash ./step1.bash
 
