b1d684dd015915fa7715a6f19825e9ffe019b14c
nishio-nobuki <nn.symmetry@outlook.com>
2017-11-08 06:06:35 +0000

    ~section7.2

--- scripts/buzzer1.py
+++ scripts/buzzer1.py
@@ -1,4 +1,12 @@
 #!/usr/bin/env python
 import rospy
-rospy.init_node('buzzer')
-rospy.spin()
+from std_msgs.msg import UInt16
+
+def recv_buzzer(data):
+    rospy.loginfo(type(data))
+    rospy.loginfo(data.data)
+
+if __name__ == '__main__':
+    rospy.init_node('buzzer')
+    rospy.Subscriber("buzzer", UInt16, recv_buzzer)
+    rospy.spin()
